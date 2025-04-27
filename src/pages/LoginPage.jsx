ef9322fd918f605d34ca1d888367778f7a5f58c3
Abu Rushed Md Bin Shams <77982841+Shams-Rushdi@users.noreply.github.com>
2025-04-27 23:19:01 +0600

    new

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Link } from 'react-router'
+import axios from 'axios'
+
+const LoginPage = () => {
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const formdata = new FormData(e.target);
+        const data = Object.fromEntries(formdata);
+
+        console.log(data);
+
+        axios.post(
+            "https://inventory-api.teamrabbil.com/api/user-login",
+            data,
+        ).then((response)=>{
+            console.log(response);
+        }).catch((error)=>{
+            console.log(error)
+        });
+
+    }
+
+  return (
+    <div>
+    <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-md-7 animated fadeIn col-lg-6 center-screen">
+                <div className="card w-90  p-4">
+                    <div className="card-body">
+                        <form  onSubmit={handleSubmit}> 
+        
+                        <h4>SIGN IN</h4>
+                        <br/>
+                        <input id="email" name="email" placeholder="User Email" className="form-control" type="email"/>
+                        <br/>
+                        <input id="password" name="password" placeholder="User Password" className="form-control" type="password"/>
+                        <br/>
+                        <button type="submit" className="btn w-100 bg-gradient-primary">Next</button>
+                        <hr/>
+                        <div className="float-end mt-3">
+                            <span>
+                                <Link className="text-center ms-3 h6" href="userRegistration.html">Sign Up </Link>
+                                <span className="ms-1">|</span>
+                                <Link className="text-center ms-3 h6" href="sendOtp.html">Forget Password</Link>
+                            </span>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default LoginPage
