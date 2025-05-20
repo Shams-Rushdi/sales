bbf46cc3175b25a76e311ec3723defe4cada64ae
Abu Rushed Md Bin Shams <77982841+Shams-Rushdi@users.noreply.github.com>
2025-05-20 22:06:11 +0600

    nn

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter,Routes, Route, Router } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -6,31 +6,33 @@ import PasswordReset from "./pages/PasswordReset";
 import PasswordResetOTP from "./pages/PasswordResetOTP";
 import SetPasswordPage from "./pages/SetPasswordPage";
 import DashboardPage from "./pages/DashboardPage";
+import DashboarLayout from "./layouts/DashboardLayout";
 import Guard from "./components/Guard";
 
+// Missing dashboard inner pages
+import DashboarIndexPage from "./pages/DashboarPage";
+import ProfilePage from "./pages/ProfilePage";
+import CustomerPage from "./pages/CustomerPage";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<IndexPage/>}/>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/register" element={<RegisterPage/>}/>
-      <Route path="/reset-password" element={<PasswordReset/>}/>
-      <Route path="/reset-password-otp" element={<PasswordResetOTP/>}/>
-      <Route path="/set-new-password" element={<SetPasswordPage/>}/>
-      
-      <Route element={<Guard/>}>
-      <Route path="/dashboard" element={<DashboardPage/>}/>
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/reset-password" element={<PasswordReset />} />
+        <Route path="/reset-password-otp" element={<PasswordResetOTP />} />
+        <Route path="/set-new-password" element={<SetPasswordPage />} />
 
-      </Route>
-      
-      
-      
-    </Routes>
+        <Route path="/dashboard" element={<DashboarLayout />}>
+          <Route path="index" element={<DashboarIndexPage />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="customer" element={<CustomerPage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
